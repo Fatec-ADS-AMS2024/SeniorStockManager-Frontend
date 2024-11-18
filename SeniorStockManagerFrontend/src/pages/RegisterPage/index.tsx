948da0e0335329486ac5ea36539d45636e5c709e
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2024-11-18 14:59:05 -0300

    :lipstick: feat: Estilizando a pagina de cadastro e deixando ela moldavel para novas alterações

--- SeniorStockManagerFrontend/src/pages/RegisterPage/index.tsx
+++ SeniorStockManagerFrontend/src/pages/RegisterPage/index.tsx
@@ -4,7 +4,8 @@ import {
 import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { routes } from "../../routes/routes";
-import Card from "../../components/Card"
+import Card from "../../components/Card";
+import SearchBar from "../../components/SearchBar";
 
 const cards = [
   {
@@ -19,25 +20,25 @@ const cards = [
     icon: <Ruler size={28} className='shrink-0' />,
     page: routes.REGISTER_UNIT_OF_MEASURE,
   },
-  
 ];
 
-
-
 export default function RegisterPage() {
-
   return (
-    <div className='grid grid-cols-5 justify-center align-middle gap-0 min-full-screen pl-12 pr-'>
-      {cards.map(({ text, icon, page, subText }) => (
-        <Card 
-          subText={subText}
-          text={text}
-          icon={icon}
-          page={page}
-        
-        />
-      
-      ))}
+    <div className="min-h-screen bg-gray-100 py-8 px-4">
+      {/* Search Bar Section */}
+      <SearchBar placeholder="Digite aqui..." action={console.log} />
+
+      {/* Card Grid */}
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {cards.map(({ text, icon, page, subText }) => (
+          <Card
+            subText={subText}
+            text={text}
+            icon={icon}
+            page={page}
+          />
+        ))}
+      </div>
     </div>
   );
 }

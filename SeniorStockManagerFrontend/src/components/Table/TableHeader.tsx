55cd8755ade74d7c80f9e0ca6df2b5d52afaee89
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2024-11-23 15:16:29 -0300

    :sparkles: feat: Adicionando o compnente tabela

---
+++ SeniorStockManagerFrontend/src/components/Table/TableHeader.tsx
@@ -0,0 +1,30 @@
+import { JSX } from "react";
+
+interface TableHeaderProps {
+  columns: string[];
+  actions?: JSX.Element;
+}
+
+export default function TableHeader({ columns, actions }: TableHeaderProps) {
+  return (
+    <thead className="text-textPrimary bg-neutral">
+      <tr className="h-12">
+        {/* Coluna reservada para as checkbox */}
+        <th className="w-10"></th>
+        {columns.map((column, index) => (
+          <th
+            key={index}
+            className="text-left"
+          >
+            {column}
+          </th>
+        ))}
+        {actions && (
+          <th className="text-center w-2/12">
+            Ações
+          </th>
+        )}
+      </tr>
+    </thead>
+  );
+}

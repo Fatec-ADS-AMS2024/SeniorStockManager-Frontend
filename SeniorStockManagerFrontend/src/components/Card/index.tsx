c023135e049bd12d556117cbfb658c6929c54783
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2024-11-23 15:08:46 -0300

    :lipstick: Feat: Estilização final do card de acordo com a padronização do projeto

--- SeniorStockManagerFrontend/src/components/Card/index.tsx
+++ SeniorStockManagerFrontend/src/components/Card/index.tsx
@@ -1,31 +1,35 @@
-import { useContext, JSX } from "react";
-import { ThemeContext } from "../../context/ThemeContext";
+import { JSX } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface CardProps {
-    text: string;
-    subText: string;
-    icon: JSX.Element;
-    page: string;
+  text: string;
+  subText: string;
+  icon: JSX.Element;
+  page: string;
 }
 
 export default function Card({ text, subText, icon, page }: CardProps) {
-    const { theme } = useContext(ThemeContext);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="w-64 h-40 bg-surface rounded-lg shadow-md border border-background cursor-pointer hover:shadow-lg transit" onClick={() => navigate(page)} >
-            <div className="h-1/4 relative ">
-                <div className="flex items-center justify-center w-12 h-12 rounded-full mb-2 text-textSecondary absolute top-3 left-3 bg-background">
-                    {icon}
-                </div>
-            </div>
-            <div className="flex justify-center items-center h-3/4 border-t">
-                <div className="z-10 flex flex-col items-center justify-center text-textPrimary">
-                    <p className="text-lg font-medium">{text}</p>
-                    <p className="text-sm text-textSecondary">{subText}</p> 
-                </div>
-            </div>
+  return (
+    <div
+      className="w-64 h-36 bg-surface rounded-lg shadow-md border border-background cursor-pointer hover:shadow-lg transition"
+      onClick={() => navigate(page)}
+    >
+      {/* Ícone */}
+      <div className="h-1/4 relative">
+        <div className="flex items-center justify-center w-14 h-14 rounded-full mb-2 text-surface p-3 absolute top-3 left-3 bg-neutralDarker">
+          {icon}
         </div>
-    );
+      </div>
+
+      {/* Conteúdo */}
+      <div className="flex items-start justify-start pt-8 mt-1 h-3/4 border-t px-4">
+        <div className="z-10 flex flex-col text-textPrimary">
+          <p className="text-lg font-medium text-left">{text}</p>
+          <p className="text-sm text-left text-textSecondary">{subText}</p>
+        </div>
+      </div>
+    </div>
+  );
 }

a75787d135677d270a4fc6a0619edbf23fec724d
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2024-12-03 13:55:20 -0300

    :lipstick: Feat: Atualização de acessibilidade para que o componente fique focado ao selecioná-lo no TAB

--- SeniorStockManagerFrontend/src/components/Card/index.tsx
+++ SeniorStockManagerFrontend/src/components/Card/index.tsx
@@ -13,8 +13,9 @@ export default function Card({ text, subText, icon, page }: CardProps) {
 
   return (
     <div
-      className="w-64 h-36 bg-neutralWhite rounded-lg shadow-md border border-neutralLighter cursor-pointer hover:shadow-lg transition"
+      className="w-64 h-36 bg-neutralWhite hover:border-r-primary rounded-lg shadow-md border border-neutralLighter cursor-pointer hover:shadow-lg transition focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
       onClick={() => navigate(page)}
+      tabIndex={0}  // Garante que o div pode ser focado
     >
       {/* Ícone */}
       <div className="h-1/4 relative">
